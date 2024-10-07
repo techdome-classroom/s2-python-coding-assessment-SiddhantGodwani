7bac1f24146e4a5370d7b250e3e89c4936250b63
Siddhant <siddhantgodwani8697@gmail.com>
2024-10-07 14:07:40 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        # Creating a stack to keep track of opening brackets
+        #Creating a stack to keep track of opening brackets
         stack = []
         #Dictionary to map closing brackets to their corresponding opening brackets
         mapping = {')': '(', '}': '{', ']': '['}
