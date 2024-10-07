12f92b54d3c011520e178e509e749bd198f812f1
Siddhant <siddhantgodwani8697@gmail.com>
2024-10-07 13:59:38 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # Dictionary to store Roman numeral values
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+                     'C': 100, 'D': 500, 'M': 1000}
         
+        # Initialize result to 0
+        total = 0
+        
+        # Traverse through the string
+        for i in range(len(s)):
+            # If the current numeral is less than the next one, subtract its value
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                # Otherwise, add its value
+                total += roman_map[s[i]]
+        
+        return total
         
         
         pass
